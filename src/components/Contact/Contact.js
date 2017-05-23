f0bc40e5ed157389c29f69a2a76ed1321a5225b6
Patrick Kosnik <patrickkosnik@Patricks-MacBook-Pro.local>
2017-05-23 01:59:11 -0400

    update to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import styles from './Contact.css'
 
 const Contact = () => (
-  <div>
-    <h1>This is Some Contact information</h1>
+  <div className={ styles.container }>
+      <h1>Contact Information</h1>
+      <div className={ styles.content }>
+      <div className={ styles.cardContainer }>
+        <div className={ styles.photoCard }></div>
+      </div>
+      <div className={ styles.cardContainer }>
+        <div className={ styles.card }>
+          <h1>Email</h1>
+          <p>patrickkosnik@gmail.com</p>
+        </div>
+      </div>
+      <div className={ styles.cardContainer }>
+        <div className={ styles.card }>
+          <h1>Phone</h1>
+          <p>(616) 666 - 1258</p>
+        </div>
+      </div>
+      <div className={ styles.cardContainer }>
+        <div className={ styles.card }>
+          <h1>LinkedIn</h1>
+          <div className={ styles.button }>
+            <a href="http://www.linkedin.com/in/patrick-kosnik-52749469">Go >></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
